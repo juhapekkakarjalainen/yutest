825117a2eae16694d9ef92c6101c43e6eb0c276e
juhapekkakarjalainen <juha.pekka.karjalainen@gmail.com>
2026-09-06 11:54:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, jsonify
 import aiml
-
-app = Flask(__name__)
-
 kernel = aiml.Kernel()
 kernel.learn("files/*.aiml")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Yleisurheiluchatbot toimii"
+    return "Hippo chatbot toimii"
 
-@app.route("/chat", methods=["POST"])
-def chat():
-    message = request.json["message"]
-    response = kernel.respond(message)
-    return jsonify({"response": response})
+@app.route("/chat/<int:ika>")
+def chat(ika):
 
-if __name__ == "__main__":
-    app.run()
+    if ika <= 6:
+        return "Voit osallistua 40 m, pituus ja pallonheitto."
+
+    elif ika <= 8:
+        return "Voit osallistua 40 m, pituus ja pallonheitto."
+
+    elif ika <= 10:
+        return "Voit osallistua 40 m, 1000 m, pituus ja kuula."
+
+    elif ika <= 12:
+        return "Voit osallistua 60 m, 1000 m, pituus ja kuula."
+
+    elif ika <= 14:
+        return "Voit osallistua 60 m, 200 m, korkeus ja pituus."
+
+    else:
+        return "Voit osallistua 100 m, 400 m, seiväs ja kuula."
