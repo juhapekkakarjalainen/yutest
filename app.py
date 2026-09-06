29d9d5114dcc8b23eb3fbef44ab73dca1bbc467d
juhapekkakarjalainen <juha.pekka.karjalainen@gmail.com>
2026-09-06 11:34:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hei maailma"
+    return "Hippo Chatbot toimii!"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
